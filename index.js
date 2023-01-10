85e30adb12777766d406511072a165cf24620ed3
Sharon Mwenda <smkathambi@gmail.com>
2023-01-10 10:01:29 +0300

    added a function that adds to do tasks

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 let todoItems = [];
 
+
+
 function addToDo (text) {
     const todo = {
         text,
@@ -11,3 +13,20 @@ function addToDo (text) {
     todoItems.push(todo)
     console.log(todoItems)
 }
+//select form element
+const form = document.querySelector(".js-form")
+
+//add a submit event listener
+form.addEventListener('submit', event => {
+    event.preventDefault()
+// select text input
+const input = document.querySelector('.js-todo-input')
+
+const text = input.value.trim()
+if (text !== ''){
+    addToDo(text)
+    input.value = ''
+    input.focus();
+}
+})
+
