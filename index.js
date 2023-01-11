922cefabf1856df2b06b868281a78c6bcec6148d
Sharon Mwenda <smkathambi@gmail.com>
2023-01-11 09:55:12 +0300

    added render and delete functions

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 
 let todoItems = [];
 
+function renderToDo (todo){
+    const list = document.querySelector(".js-todo-list")
 
+    const item = document.querySelector(`[data-key='${todo.id}']`)
+
+    if(todo.deleted) {
+        //remove item from the DOM
+        item.remove();
+        return
+    }
+    
+    const isChecked = todo.checked ? 'done' : '';
+
+    const node = document.createElement("li");
+
+    node.setAttribute('class', `todo-item ${isChecked}`)
+    node.setAttribute('data-key', todo.id)
+
+    node.innerHTML = `
+    <input id='${todo.id}' type= 'checkbox' />
+    <label for = '${todo.id}' class = 'tick js-tick'></label>
+    <span> ${todo.text}</span>
+    <button class= 'delete-todo js-delete-todo'>
+    <svg><use href='#delete-icon'</use></svg>
+    </button>
+    `
+    //if the item already exists in the DOM
+    if (item) {
+        //replace it
+        list.replaceChild(node, item)
+    } else {
+        //otherwise append it to the end of the list
+        list.append(node)
+    }
+}
 
 function addToDo (text) {
     const todo = {
@@ -11,7 +45,28 @@ function addToDo (text) {
     };
 
     todoItems.push(todo)
-    console.log(todoItems)
+    renderToDo(todo)
+}
+
+function toggleDone(key) {
+    //findIndex is an array method that returns the position of an element
+    const index = todoItems.findIndex(item => item.id === Number(key));
+
+    todoItems[index].checked =! todoItems[index].checked;
+    renderToDo(todoItems[index]);
+}
+
+function deleteToDo(key) {
+    const index = todoItems.findIndex(item => item.id === Number(key));
+
+    const todo = {
+        deleted: true,
+        ...todoItems[index]
+    };
+    //remove the todo item from the array by filtering it out
+
+    todoItems = todoItems.filter(item => item.id !== Number(key))
+    renderToDo(todo);
 }
 //select form element
 const form = document.querySelector(".js-form")
@@ -28,5 +83,20 @@ if (text !== ''){
     input.value = ''
     input.focus();
 }
+})
+
+//mark a task complete
+const list = document.querySelector('.js-todo-list');
+//adding an event listener
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('js-tick')){
+        const itemKey = e.target.parentElement.dataset.key;
+        toggleDone(itemKey)
+    }
+
+    if(e.target.classList.contains('js-delete-todo')) {
+        const itemKey = e.target.parentElement.dataset.key;
+        deleteToDo(itemKey)
+    }
 })
 
